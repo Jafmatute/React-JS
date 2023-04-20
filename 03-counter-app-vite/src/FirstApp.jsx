cf5cd8af776587e58a1409e897b94e4c22acf94b
codewithJosue <repositorydevcode@gmail.com>
2023-04-20 09:06:47 -0600

    impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,16 @@
 
+const newMessage = 'Josue';
+
+const getMessage = () => {
+
+    return 'React'
+}
 const FirstApp = () => {
 
     return (
         <>
-            <h1>First App</h1>
-            <p>subtitulo para poder mostrar en el html</p>
+            <h1>First App {getMessage()}</h1>
+            <p> este es un subtitulo para poder mostrar en el html</p>
         </>
     )
 }
