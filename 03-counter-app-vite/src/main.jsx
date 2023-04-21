8061d9f5f6ce30c9e2ad29d97a832807166fb2b6
codewithJosue <repositorydevcode@gmail.com>
2023-04-21 08:35:50 -0600

    Instalación de React Testing Library

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import CounterApp from './CounterApp.jsx'
 
 import './style.css';
+import FirstApp from "./FirstApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <CounterApp value={10}  />
+        {/*<CounterApp value={10}  />*/}
+        <FirstApp />
     </React.StrictMode>
 )
 
