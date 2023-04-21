ddd827740995aa391264793b92989676cf40a575
codewithJosue <repositorydevcode@gmail.com>
2023-04-21 08:19:13 -0600

    evaluar el catch de async await

--- 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -9,4 +9,14 @@ describe('Pruebas en el archivo 11-async-await.js', ()=> {
         expect(typeof url).toBe('string');
 
     })
+
+    test('getImagen debe de retornar un error si no hay API KEY', async ()=> {
+
+        const rsp = await getImagen();
+
+        expect(rsp).toBe('No se encontro la imagen');
+
+    })
+
+
 })
