a24930e5a6143fbc05fc0bfb31ebb57ecafa9872
codewithJosue <repositorydevcode@gmail.com>
2023-04-21 14:50:13 -0600

    creando una lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
-const GifExpertApp = (props) => (
-    <>
-    <h1>GifExpertApp </h1>
-    </>
-);
+import {useState} from "react";
+
+const GifExpertApp = (props) => {
+    const [categories, setCategories] = useState(['One Punch', 'Demon Slayer']);
+
+    return(
+        <>
+            <h1>GifExpertApp </h1>
+
+            <ol>
+                {categories.map((category,index) => {
+                    return <li key={index}>{category}</li>
+                })}
+            </ol>
+        </>
+    )
+
+
+};
 
 export default GifExpertApp;
