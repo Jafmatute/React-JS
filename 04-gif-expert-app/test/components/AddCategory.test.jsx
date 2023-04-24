3f0c43a4e2e7d3ccf44639970076a7a56685a342
codewithJosue <repositorydevcode@gmail.com>
2023-04-24 14:45:47 -0600

    emitir un evento submit

--- 04-gif-expert-app/test/components/AddCategory.test.jsx
+++ 04-gif-expert-app/test/components/AddCategory.test.jsx
@@ -12,6 +12,21 @@ describe('Pruebas en el componente <AddCategory />', ()=> {
 
         expect(input.value).toBe('saitama');
 
+    })
+
+    test('debe de llamar onNewCategory si el input tiene un valor', ()=> {
+
+        const inputValue = 'saitama';
+
+        render(<AddCategory onNewCategory={()=> {} }/>);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, {target:{value: inputValue }});
+        fireEvent.submit(form);
+
+        expect(input.value).toBe('');
 
     })
 })
