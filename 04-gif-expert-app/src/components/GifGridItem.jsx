068f2d39fab30ed48e19711b93ec64cf54ee638d
codewithJosue <repositorydevcode@gmail.com>
2023-04-24 13:43:32 -0600

    Pruebas en el componente - GifGridItem

--- 04-gif-expert-app/src/components/GifGridItem.jsx
+++ 04-gif-expert-app/src/components/GifGridItem.jsx
@@ -4,6 +4,7 @@ export const GifGridItem = ({title, url}) => (
 
 <div className="card">
 <img src={ url } alt={title} />
+    <p>{title}</p>
 </div>
 
 );
--- 04-gif-expert-app/test/components/GifGridItem.test.jsx
+++ 04-gif-expert-app/test/components/GifGridItem.test.jsx
@@ -1,10 +1,10 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {GifGridItem} from "../../src/components/index.js";
 
 describe('Pruebas en el componente <GifGridItem />',()=> {
 
     const title = 'Google';
-    const url = 'www.google.com';
+    const url = 'http://localhost/www.google.com';
 
     test('debe hacer match con el snapshot', ()=> {
 
@@ -12,5 +12,24 @@ describe('Pruebas en el componente <GifGridItem />',()=> {
 
         expect(container).toMatchSnapshot();
 
+    });
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', ()=> {
+
+        render(<GifGridItem title={title} url={url} />);
+
+        const {src, alt} = screen.getByRole('img');
+        expect( src ).toBe(url);
+        expect( alt ).toBe(title);
+
     })
+
+    test('debe de mostrar el titulo en el componente', ()=> {
+
+        render(<GifGridItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
+
+    })
+
+
 })
