3f65cecde0b108612ff708d31a3583ef3d482a06
codewithJosue <repositorydevcode@gmail.com>
2023-04-29 09:23:55 -0600

    renderizando los valores del arreglo en el html

--- 05-hook-app/src/03-examples/MultipeCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipeCustomHooks.jsx
@@ -4,11 +4,29 @@ const MultipeCustomHooks = () => {
 
     const {data, isLoading, hasError} = useFetch(' https://api.breakingbadquotes.xyz/v1/quotes/1');
 
-    console.log(data, isLoading, hasError)
+    const {author, quote} = !!data && data[0];
+
     return (
         <>
         <h1>BreakingBad Quotes</h1>
             <hr />
+
+            {
+                isLoading ?
+                    (
+                        <div className='alert alert-info'>
+                            Loading.....
+                        </div>
+                    ):
+                    (
+                        <blockquote className='blockquote text-end'>
+                            <p className='mb-1'>{quote}</p>
+                            <footer className='blockquote-footer mt-1'>{author}</footer>
+                        </blockquote>
+                    )
+            }
+
+
         </>
     )
 }
