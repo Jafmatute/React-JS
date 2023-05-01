4f9cfc6e68c3ba37cddf2fa2a9981d205fd15bae
codewithJosue <repositorydevcode@gmail.com>
2023-05-01 08:56:02 -0600

    useMemo

---
+++ 05-hook-app/src/06-memos/MemoHook.jsx
@@ -0,0 +1,33 @@
+import {useCounter} from "../hooks/index.js";
+import {useMemo, useState} from "react";
+
+const heavyStuff = (iterationNumber =100) => {
+
+    for (let i = 0; i < iterationNumber; i++) {
+        console.log('ahí vamos...')
+    }
+
+    return `${iterationNumber} iteraciones realizadas`;
+}
+
+export const MemoHook = () => {
+
+    const {counter, increment} = useCounter(4000);
+    const [show, setShow] = useState(true);
+
+    const messageMemorize = useMemo(()=> heavyStuff(counter), [counter])
+
+    return (
+
+        <>
+        <h1>Counter: <small>{counter}</small></h1>
+            <hr />
+
+            <h4>{messageMemorize}</h4>
+
+            <button className='btn btn-primary' onClick={()=> increment(1)} >+1</button>
+            <button className='btn btn-outline-primary' onClick={()=> setShow(!show)} >Show/Hide {JSON.stringify(show)}</button>
+        </>
+
+    )
+}
