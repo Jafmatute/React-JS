c52dfa333547b5bbe7701a4669538031ecc3a4fb
codewithJosue <repositorydevcode@gmail.com>
2023-05-01 09:20:48 -0600

    useCallback

---
+++ 05-hook-app/src/06-memos/CallbackHook.jsx
@@ -0,0 +1,24 @@
+import {useCallback, useState} from "react";
+import {ShowIncrement} from "../components/ShowIncrement.jsx";
+
+export const CallbackHook = () => {
+
+    const [counter, setCounter] = useState(10);
+
+    const incrementFather =useCallback(
+        () => {
+            setCounter(value => value +1);
+        },
+        [],
+    );
+
+
+    //const incrementFather = () => setCounter(counter+1);
+
+    return (
+        <>
+        <h1>useCallback Hook: {counter}</h1>
+            <ShowIncrement increment={incrementFather} />
+        </>
+    )
+}
